df272fb29ca9a77f35d2f52a6c0e8b43372539e1
TheGCoder <marbros_02@outlook.com>
2025-10-15 11:24:24 -0600

    Se formaliza más el proyecto

---
+++ inventario/Data/AppDBContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using inventario.Models;
+
+namespace inventario.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> opciones) : base(opciones)
+        {
+
+        }
+        //Se defina las tablas con dbset para ser creadas en la base de datos
+        public DbSet<Usuario> Usuarios { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region creacionTablaUsuario
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Usuario>(tb =>
+            {
+                //se configura la primera columna de la base de datos  
+                tb.HasKey(col => col.Id);
+                tb.Property(col => col.Id).UseIdentityColumn().ValueGeneratedOnAdd();
+
+                //
+                tb.Property(col => col.Nombre).HasMaxLength(40).IsRequired();
+                tb.Property(col => col.ApellidoMaterno).HasMaxLength(40).IsRequired();
+                tb.Property(col => col.ApellidoPaterno).HasMaxLength(40).IsRequired();
+                tb.Property(col => col.Email).HasMaxLength(50).IsRequired();
+                tb.Property(col => col.Salt).HasMaxLength(16).IsRequired();
+                tb.Property(col => col.Status).IsRequired();
+                tb.Property(col => col.Rol).HasConversion<int>().IsRequired();
+                tb.Property(col => col.FechaNacimiento).IsRequired();
+
+
+            });
+            modelBuilder.Entity<Usuario>().ToTable("Usuario");
+        #endregion
+        }
+
+
+    }
+    
+}
---
+++ inventario/Models/Usuario.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace inventario.Models
+{
+    
+    public enum Rol
+    {
+        Usuario = 0,
+        Administrador = 1 
+    }
+    public class Usuario
+    {
+       
+        public int Id { get; set; }
+        [MaxLength(40)]
+        [Required]
+        public string Nombre { get; set; }
+        [MaxLength(40)]
+        [Required]
+        public string ApellidoMaterno { get; set; }
+        [MaxLength(40)]
+        [Required]
+        public string ApellidoPaterno { get; set; }
+        [MaxLength(50)]
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [RegularExpression(@"(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$",
+        ErrorMessage = "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un número o símbolo.")]
+        [DataType(DataType.Password)]
+        public string Pass { get; set; }
+        [Required]
+        public string Salt { get; set; }
+        [Required]
+        public char Status { get; set; } = 'A'; //Dropdownlist (A/I/B)
+        [Required]
+        public int Rol { get; set; }
+        [Required]
+        public DateTime FechaAlta { get; set; } = DateTime.Today;
+        [DataType(DataType.DateTime)]     
+        [Required]
+        public DateTime FechaNacimiento { get; set; } 
+    }
+}
